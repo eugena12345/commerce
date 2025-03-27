4b1edff4b3d82d8eb5b8b203dc102845b6ca8ceb
Eugena <eugena.h12345@gmail.com>
2025-03-27 15:37:34 +0300

    add header layout

--- src/App/components/Header/Header.tsx
+++ src/App/components/Header/Header.tsx
@@ -1,11 +1,29 @@
 import React from "react";
+import styles from './Header.module.css';
+import logo from './Frame.svg';
+import lalasiaLogo from './Lalasia.svg';
+import bag from './bag-2.svg';
+import userSvg from './user.svg';
 
 const Header = () => {
     return (
-        <div>
-            <div>Logo</div>
-            <div>info</div>
-            <div>UserInfo</div>
+        <div className={styles.container}>
+            <div className={styles.logoContainer}>
+                <img src={logo} alt='logo' className={styles.logo} />
+                <img src={lalasiaLogo} alt='Lalasia' className={styles.logo} />
+            </div>
+            <nav className={styles.headerNav}>
+                <ul>
+                    <li><a href="/products">Products</a></li>
+                    <li><a href="/categories">Categories</a></li>
+                    <li><a href="/about">About us</a></li>
+                </ul>
+            </nav>
+
+            <div className={styles.userInfoContainer}>
+            <img src={bag} alt='bag' className={styles.userInfo} />
+            <img src={userSvg} alt='userSvg' className={styles.userInfo} />
+            </div>
         </div>
     )
 }
