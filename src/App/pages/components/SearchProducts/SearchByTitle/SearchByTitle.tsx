f515b309993057572e15f2ab63f1244b61c64eb3
Eugena <eugena.h12345@gmail.com>
2025-04-06 20:38:47 +0300

    add searchValueStore

--- src/App/pages/components/SearchProducts/SearchByTitle/SearchByTitle.tsx
+++ src/App/pages/components/SearchProducts/SearchByTitle/SearchByTitle.tsx
@@ -1,32 +1,33 @@
 import Input from "App/pages/components/Input/Input";
 import Button from "components/Button/Button";
 import styles from './SearchByTitle.module.scss';
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect } from "react";
 import { useSearchParams } from "react-router";
 import qs from "qs";
-import { observer } from "mobx-react-lite";
+import { observer, useLocalStore } from "mobx-react-lite";
 import { useStoreContext } from "App/App";
+import SearchValueStore from './../../../../../store/SearchValueStore/SearchValueStore';
 
 const SearchByTitle = observer(() => {
-    const [searchQuery, setSearchQuery] = useState("");
     const [searchParams, setSearchParams] = useSearchParams();
     const queryStore = useStoreContext();
+    const valueStore = useLocalStore(() => new SearchValueStore({ valueDefault: '' }))
 
     useEffect(() => {
         const parsedParams = qs.parse(searchParams.toString(), { decode: true });
         if (parsedParams.filters?.title) {
-            setSearchQuery(parsedParams.filters.title.$containsi)
+            valueStore.setValue(parsedParams.filters.title.$containsi)
         }
-    }, [searchParams]);
+    }, [searchParams, valueStore]);
 
     const handleInputChange = (searchQuery: string) => {
-        setSearchQuery(searchQuery);
+        valueStore.setValue(searchQuery);
     };
 
     const handleSearch = useCallback(() => {
         const newFilter = {
             title: {
-                $containsi: searchQuery,
+                $containsi: valueStore.value,
             },
         };
         queryStore.query.setPage(1);
@@ -37,12 +38,12 @@ const SearchByTitle = observer(() => {
         queryStore.query.updateUrl((queryString: string) => {
             setSearchParams(queryString);
         });
-    }, [queryStore, searchQuery, setSearchParams]
+    }, [queryStore.query, setSearchParams, valueStore.value]
     )
 
     return (
         <div className={styles['container__search']}>
-            <Input placeholder="Search product" onChange={handleInputChange} value={searchQuery} className={styles['container__search--grow']} />
+            <Input placeholder="Search product" onChange={handleInputChange} value={valueStore.value} className={styles['container__search--grow']} />
             <Button onClick={handleSearch}>Find now</Button>
         </div>
     )
