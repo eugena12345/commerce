a2967cf14526ea1b91b7a8bd5672508df936cc3d
Eugena <eugena.h12345@gmail.com>
2025-04-06 16:52:14 +0300

    add RootStore

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router';
 import Header from '../components/Header'
 import styles from './App.module.scss';
+import { useQueryParamsStoreInit } from '../store/RootStore/hooks/useQueryParamsStoreInit';
 
 function App() {
+  useQueryParamsStoreInit();
+
   return (
     <div className={styles.app}>
       <Header/>
