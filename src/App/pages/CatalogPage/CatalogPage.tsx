2628dd65d809c5d47b71dc46b8a5e13eaeb4b80e
Eugena <eugena.h12345@gmail.com>
2025-04-07 13:17:55 +0300

    rename store and remove unnecessary callback

--- src/App/pages/CatalogPage/CatalogPage.tsx
+++ src/App/pages/CatalogPage/CatalogPage.tsx
@@ -10,32 +10,27 @@ import Pagination from "App/pages/components/Pagination/Pagination";
 import { observer, useLocalStore } from "mobx-react-lite";
 import CatalogStore from "./../../../store/CatalogStore/CatalogStore"; //  TODO добавить алиас
 import qs from "qs";
-import { ParamsFromQuery } from '../../../store/CatalogStore/types';
 import { useStoreContext } from "./../../../store/RootStore/context/rootStoreContext";
 
 const CatalogPage = observer(() => {
 
     const catalogStore = useLocalStore(() => new CatalogStore());
     const [searchParams, setSearchParams] = useSearchParams();
-    const queryStore1 = useStoreContext();
+    const rootStore = useStoreContext();
 
     useEffect(() => {
         const parsedParams = qs.parse(searchParams.toString(), { decode: true });
-        if (parsedParams.page) queryStore1.query.setPage(Number(parsedParams.page));
-        if (parsedParams.filters) queryStore1.query.setFilters(parsedParams.filters as Record<string, any>);//TODO записать правильную типизацию
-    }, [catalogStore, queryStore1, searchParams]); 
+        if (parsedParams.page) rootStore.query.setPage(Number(parsedParams.page));
+        if (parsedParams.filters) rootStore.query.setFilters(parsedParams.filters as Record<string, any>);//TODO записать правильную типизацию
+    }, [catalogStore, rootStore, searchParams]);
 
     const navigate = useNavigate();
     const navigaveToProductPage = (documentId: string) => navigate(routes.product.create(documentId))
 
-    const getProductByCategory = (params: ParamsFromQuery): void => {
-        catalogStore.getProducts(params);
-    }
-
     const resetFilter = (): void => {
-        queryStore1.query.setPage(1);
-        queryStore1.query.setFilters({});
-        queryStore1.query.updateUrl((queryString: string) => {
+        rootStore.query.setPage(1);
+        rootStore.query.setFilters({});
+        rootStore.query.updateUrl((queryString: string) => {
             setSearchParams(queryString);
         });
     }
@@ -47,7 +42,6 @@ const CatalogPage = observer(() => {
                 {catalogStore.items.length > 0 &&
                     <SearchProducts
                         totalItems={catalogStore.metaInfo.pagination.total}
-                        callbackOnFilter={getProductByCategory}
                     />
                 }
                 <Button onClick={resetFilter} >Reset filter</Button>
