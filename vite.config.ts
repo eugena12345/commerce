bec1736558f8daefe2d66a358f65c9685c5c04b9
Eugena <eugena.h12345@gmail.com>
2025-04-21 18:37:14 +0300

    update settings

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ const parseTsConfigPaths = (paths: Record<string, string[]>): Record<string, str
 };
 
 export default defineConfig({
-  base: 'https://eugena12345.github.io/commerce', 
+  base: '/commerce/', 
   plugins: [react()],
   resolve: {
     alias: parseTsConfigPaths(tsconfig.compilerOptions.paths),
